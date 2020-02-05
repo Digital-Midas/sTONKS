cf3f226bc0e7c47ca324c26c31796df5dc01a287
Alexandr-star <sanya-bass.skvortzoff@yandex.ru>
2020-02-05 16:05:54 +0300

    my work is slow, but I create checks

--- stock_database/stokdb.py
+++ stock_database/stokdb.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, ForeignKey, REAL
+import sqlalchemy
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, ForeignKey, DECIMAL
+import psycopg2
 
 
 def connect(user, password, database, host='localhost', port='5432'):
@@ -6,28 +8,67 @@ def connect(user, password, database, host='localhost', port='5432'):
     url = 'postgresql+psycopg2://{}:{}@{}:{}/{}'
     url = url.format(user, password, host, port, database)
 
-    con = create_engine(url, client_encoding='utf8')
-    meta_data = MetaData(bind=con, reflect=True)
+    try:
+        con = create_engine(url, client_encoding='utf8')
+        meta_data = MetaData(bind=con, reflect=True)
+    except sqlalchemy.exc.OperationalError:
+        print("Database doesn't exists or username/password incorrect.")
+        return False, False
+    else:
+        return con, meta_data
 
-    return con, meta_data
+
+def create_table(meta_data):
+    try:
+        stoks = Table(
+            'stoks', meta_data,
+            Column('id_org', Integer, primary_key=True),
+            Column('id_company', Integer),
+            Column('org_name', String),
+            Column('date', Date),
+            Column('stock_price', DECIMAL)
+        )   
+
+        analytics = Table(
+            'analytics', meta_data,
+            Column('id_anal', Integer, ForeignKey('stoks.id_org')),
+            Column('name', String),
+            Column('rating', DECIMAL)
+        )
+
+        markets = Table(
+            'markets', meta_data,
+            Column('id_market', Integer),
+            Column('name', String),
+            Column('linkname', String)
+        )
+
+        company = Table(
+            'company', meta_data,
+            Column('id_compamy', Integer),
+            Column('name', String),
+            Column('linkname', String),
+            Column('id_market', Integer)
+        )
+    except sqlalchemy.exc.InvalidRequestError:
+        print("All table is already.")
+        return meta_data.tables['stoks'], meta_data.tables['analytics'], \
+            meta_data.tables['markets'], meta_data.tables['company']
+    else:
+        return stoks, analytics, markets, company
 
 
 con, meta_data = connect('stonkboy', 'stonks228', 'stonks_db')
 
-stoks = Table(
-    'stoks', meta_data,
-    Column('id_org', Integer, primary_key=True),
-    Column('org_name', String),
-    Column('date', Date),
-    Column('stock_price', REAL)
-)
-analytics = Table(
-    'analytics', meta_data,
-    Column('id', Integer, ForeignKey('stoks.id_org')),
-    Column('name', String),
-    Column('rating', REAL)
-)
+if con != False: 
+    stoks, analytics, markets, company =  create_table(meta_data)
 
-meta_data.create_all(con)
+    meta_data.create_all(con)
+    print("Connect successful! You're awesome!")       
+else:
+    print("Connect is poo!")
+
+
+
 
 
